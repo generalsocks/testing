05aec99f91ecb2a853a4d71a91bed9721c3a2ae8
Peter van Zyl <Peter.vanZyl@momentum.co.za>
2024-04-25 16:09:29 +0200

    breaking sync-ceremony

--- src/sync-ceremony/sync-ceremony.component.ts
+++ src/sync-ceremony/sync-ceremony.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-sync-ceremony',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './sync-ceremony.component.html',
   styleUrl: './sync-ceremony.component.css'
 })
 export class SyncCeremonyComponent {
 
 }
+
