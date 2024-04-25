9cae74cdc5d7c17bf5f52ddd986169acaf448ddf
Peter van Zyl <Peter.vanZyl@momentum.co.za>
2024-04-25 16:33:29 +0200

    moved syncceremony under app folder

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -3,13 +3,13 @@ import { FeaturesComponent } from './app/features/features.component';
 import {HomeComponent} from './app/home/home.component'
 import { LoginComponent } from './app/login/login.component';
 import { Routes } from '@angular/router';
-import { SyncCeremonyComponent } from './app/sync-ceremony/sync-ceremony.component';
+import { SyncCeremonyComponent } from './app/syncceremony/syncceremony.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
     {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
     {path: 'features', component: FeaturesComponent, canActivate: [AuthGuard]},
-    {path: 'sync-ceremony', component: SyncCeremonyComponent, canActivate: [AuthGuard]},
+    {path: 'syncceremony', component: SyncCeremonyComponent, canActivate: [AuthGuard]},
     {path: '**', redirectTo: 'login'}
 ];
