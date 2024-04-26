5c6af55a59ab8796d13ad1da58f5968e9e1980e1
Peter van Zyl <Peter.vanZyl@momentum.co.za>
2024-04-26 10:41:59 +0200

    sync ceremony page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,14 @@ import { FeaturesComponent } from './features/features.component';
 import {HomeComponent} from './home/home.component'
 import { LoginComponent } from './login/login.component';
 import { Routes } from '@angular/router';
-import { SyncCeremonyComponent } from '../sync-ceremony/sync-ceremony.component';
+import { SyncCeremonyComponent } from './syncceremony/syncceremony.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'login', component: LoginComponent}, 
     {path: 'home', component: HomeComponent, canActivate: [AuthGuard]}, 
     {path: 'features', component: FeaturesComponent, canActivate: [AuthGuard]},
-    {path: 'sync-ceremony', component: SyncCeremonyComponent, canActivate: [AuthGuard]},
+    {path: 'syncceremony', component: SyncCeremonyComponent, canActivate: [AuthGuard]},
     {path: '**', redirectTo: 'login'}
 ];
 
