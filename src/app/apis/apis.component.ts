bddd7f4104229fbd62b8907944f781666cd99592
Ashish Prajapati <ashish.prajapati@mmltd.co.za>
2024-04-29 20:08:17 +0530

    added async and await code for smooth transition

--- src/app/apis/apis.component.ts
+++ src/app/apis/apis.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { ApiDataService } from '../../services/api-data.service';
 import { CommonModule } from '@angular/common';
@@ -36,18 +36,25 @@ export class ApisComponent implements OnInit {
   searchForm = new FormGroup({
     searchInput: new FormControl('')
  });
+ userForm!: FormGroup; 
  
 
   constructor(
     public networkService: NetworkService,
     private apiDataService: ApiDataService,
     public pouchdbService: PouchdbService, 
-    public networkStatus: NetworkService
+    public networkStatus: NetworkService,
+    private _fb:FormBuilder
   ) {}
 
   async ngOnInit(): Promise<void> {
+    this.CreateForm();
     await this.getData();
     this.networkService.checkNetworkStatus();
+    this.selUserToDelete = '0';
+    this.selectedIdForUpdate = '0';
+    this.selectedFieldForUpdate = '0';
+    this.textForUpdate="";
   }
 
   ngOnDestroy(): void {
@@ -55,18 +62,24 @@ export class ApisComponent implements OnInit {
     this.networkService.networkStatus.valueOf;
   }
   async getData() {
+    if(navigator.onLine)
     (await this.apiDataService.getPosts()).subscribe(async (posts) => {
       this.posts = posts;
       await this.pouchdbService.bulkInsert(this.pouchdbService.apidb, posts);
+      
+    });
+    else
+    {
       await this.pouchdbService
         .fetchAllDocs(this.pouchdbService.apidb)
         .then((docs) => {
-          console.log("Fetching all docs",docs);
+          this.posts=docs;
+          // console.log("Fetching all docs",docs);
         })
         .catch((err) => {
           console.log(err);
         });
-    });
+    }
   } 
 
   async findPost(id: string) {
@@ -79,17 +92,38 @@ export class ApisComponent implements OnInit {
    });
   } 
 
-  deleteUser(id: string){
+  async deleteUser(id: string){
     if (id == '') {
       swal("no record was selected for deletion");
     } 
     else {
       console.log("Deleting post with id:", id);
-      this.pouchdbService.DB_DeleteById(this.pouchdbService.apidb, id);
+      await this.pouchdbService.DB_DeleteById(this.pouchdbService.apidb, id);
+      await this.pouchdbService
+        .fetchAllDocs(this.pouchdbService.apidb)
+        .then((docs) => {
+          this.posts=docs;
+          this.selUserToDelete="0";
+          swal("Data is deleted successfully");
+          //console.log("Fetching all docs",docs);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     } 
   }
   async updatePost(id: string, field: string, newValue: string) {
     console.log("Updating the record with id:", id); 
+    if(id=="0")
+      {
+        swal("Please select the id");
+        return;
+      }
+      if(field=="0")
+        {
+          swal("Please select the field");
+          return;
+        }
     console.log("Field to update:", field);
     let changedData = {};
     if(field == 'name') {
@@ -106,7 +140,55 @@ export class ApisComponent implements OnInit {
       }
     }
    
-     this.pouchdbService.DB_UpdateById(this.pouchdbService.apidb, id, changedData);
+    await this.pouchdbService.DB_UpdateById(this.pouchdbService.apidb, id, changedData);
+    await this.pouchdbService
+        .fetchAllDocs(this.pouchdbService.apidb)
+        .then((docs) => {
+          this.posts=docs;
+          this.selectedIdForUpdate = '0';
+          this.selectedFieldForUpdate = '0';
+          this.textForUpdate="";
+          swal("Data is updated successfully");
+          //console.log("Fetching all docs",docs);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
   }
- 
+
+//create form model
+private CreateForm() {
+  this.userForm = this._fb.group({
+    name: ['', Validators.required],
+    username: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]]
+  });
+}
+
+  InsertData()
+  {
+    if(this.userForm.invalid)
+      {
+        swal("Please fill all the required fields");
+        return;
+      }
+
+      let data=this.userForm.getRawValue();
+      console.log(data);
+      this.pouchdbService.apidb.post(data).then(res=>{
+        swal("Data is inserted successfully");
+        this.userForm.reset();
+        this.pouchdbService
+        .fetchAllDocs(this.pouchdbService.apidb)
+        .then((docs) => {
+          this.posts=docs;
+          //console.log('Fetching all docs', docs);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      }).catch(err=>{
+        swal("Error while inserting the data");
+      })
+  }
 }
